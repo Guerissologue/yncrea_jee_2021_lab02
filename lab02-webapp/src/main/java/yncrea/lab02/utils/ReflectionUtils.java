ea3a46a9a47662cd9892c2c8a4e8719827f58a1b
Amaury WILLEMANT <amaury.willemant@worldline.com>
2020-08-22 15:13:46 +0200

    update 2021

--- lab02-webapp/src/main/java/yncrea/lab02/utils/ReflectionUtils.java
+++ lab02-webapp/src/main/java/yncrea/lab02/utils/ReflectionUtils.java
@@ -1,18 +1,32 @@
 package yncrea.lab02.utils;
 
 import yncrea.lab02.core.dao.QuoteProvider;
+import yncrea.lab02.core.service.QuoteService;
+
+import java.lang.reflect.InvocationTargetException;
 
 public final class ReflectionUtils {
 
     public static QuoteProvider getQuoteProviderInstanceFromClassName(final String className){
         try {
             final Class<?> providerClass = Class.forName(className);
-            return (QuoteProvider) providerClass.newInstance();
+            return (QuoteProvider) providerClass.getDeclaredConstructor().newInstance();
 
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+
+    public static QuoteService buildQuoteService(String className, QuoteProvider provider) {
+        try {
+            final Class<?> providerClass = Class.forName(className);
+            return (QuoteService) providerClass.getDeclaredConstructor(QuoteProvider.class).newInstance(provider);
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
